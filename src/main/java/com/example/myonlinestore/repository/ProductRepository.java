457c90e473860a44e6f850a8fcf64257246be1f8
Pate, Rajvi <rajvpate@visa.com>
2025-08-27 16:23:47 -0700

    Test commit

--- src/main/java/com/example/myonlinestore/repository/ProductRepository.java
+++ src/main/java/com/example/myonlinestore/repository/ProductRepository.java
@@ -3,5 +3,6 @@ package com.example.myonlinestore.repository;
 import com.example.myonlinestore.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//Added a comment for GitPush
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
