8858e3cc43dd4f5bac6e87f036c5dbfbf69aeae7
Pate, Rajvi <rajvpate@visa.com>
2025-08-27 14:19:14 -0700

    Initial Commit

---
+++ src/main/java/com/example/myonlinestore/MyOnlineStore.java
@@ -0,0 +1,11 @@
+package com.example.myonlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOnlineStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyOnlineStoreApplication.class, args);
+    }
+}
