78f5b9b64b5049a5b84f1cdfd03748fe06e0d1a4
Pate, Rajvi <rajvpate@visa.com>
2025-08-27 18:50:54 -0700

    changed folder name to my-online-store

--- src/main/java/com/example/myonlinestore/MyOnlineStoreApplication.java
+++ src/main/java/com/example/myonlinestore/MyOnlineStoreApplication.java
@@ -3,6 +3,8 @@ package com.example.myonlinestore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Added a comment for GitPush
+
 @SpringBootApplication
 public class MyOnlineStoreApplication {
     public static void main(String[] args) {
